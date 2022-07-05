83f07491c9202dc3554db809c37600c05813d2e8
Romina-Ramirez <romina.maite.rt@gmail.com>
2022-07-04 20:17:11 -0500

    Taller 17 - JPA y Mapeo de Secuencias

--- src/main/java/com/uce/edu/demo/ProyectoU2RrApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2RrApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.demo.estudiante.repository.modelo.Estudiante;
 import com.uce.edu.demo.estudiante.service.IEstudianteJpaService;
+import com.uce.edu.demo.repository.modelo.Persona;
+import com.uce.edu.demo.service.IPersonaJpaService;
 
 @SpringBootApplication
 public class ProyectoU2RrApplication implements CommandLineRunner {
@@ -15,7 +16,7 @@ public class ProyectoU2RrApplication implements CommandLineRunner {
 	private static final Logger logger = Logger.getLogger(ProyectoU2RrApplication.class);
 
 	@Autowired
-	private IEstudianteJpaService iEstudianteJpaService;
+	private IPersonaJpaService iPersonaJpaService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2RrApplication.class, args);
@@ -25,37 +26,27 @@ public class ProyectoU2RrApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 
-		Estudiante estudiante = new Estudiante();
-		estudiante.setId("7");
-		estudiante.setNombre("Romina");
-		estudiante.setApellido("Ramírez");
-		estudiante.setDireccion("Av. Independencia");
-		estudiante.setTelefono("0968527182");
+		Persona persona = new Persona();
+		persona.setNombre("Luis");
+		persona.setApellido("Ramírez");
 
-		Estudiante estudiante1 = new Estudiante();
-		estudiante1.setId("8");
-		estudiante1.setNombre("Evelyn");
-		estudiante1.setApellido("Loayza");
-		estudiante1.setDireccion("Av. Kennedy");
-		estudiante1.setTelefono("0987462514");
-
-		Estudiante estudiante2 = new Estudiante();
-		estudiante2.setId("9");
+		Persona persona1 = new Persona();
+		persona1.setNombre("Sana");
+		persona1.setApellido("Lopez");
 
 		// Guardar
-		this.iEstudianteJpaService.guardar(estudiante);
-		this.iEstudianteJpaService.guardar(estudiante1);
-		this.iEstudianteJpaService.guardar(estudiante2);
-
-		// Buscar por Id
-		logger.info(this.iEstudianteJpaService.buscarPorId("9"));
+		this.iPersonaJpaService.guardar(persona);
+		this.iPersonaJpaService.guardar(persona1);
+		
+		//Leer
+		//this.iPersonaJpaService.buscarPorId(10);
 
 		// Actualizar
-		estudiante1.setTelefono("0987462524");
-		this.iEstudianteJpaService.actualizar(estudiante1);
+		persona1.setNombre("Maite");
+		//this.iPersonaJpaService.actualizar(persona1);
 
 		// Eliminar
-		this.iEstudianteJpaService.eliminarPorId("9");
+		//this.iPersonaJpaService.eliminarPorId(11);
 
 	}
 
