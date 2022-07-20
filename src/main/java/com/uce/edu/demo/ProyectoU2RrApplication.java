3064deb9153eaeb2f6a0b110beb4d674fc22965f
Romina-Ramirez <romina.maite.rt@gmail.com>
2022-07-19 20:09:59 -0500

    Taller 24 - Mapeo JPA OneToOne

--- src/main/java/com/uce/edu/demo/ProyectoU2RrApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2RrApplication.java
@@ -1,6 +1,6 @@
 package com.uce.edu.demo;
 
-import java.util.List;
+import java.math.BigDecimal;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.demo.estudiante.repository.modelo.EstudianteContadorSemestre;
-import com.uce.edu.demo.estudiante.repository.modelo.EstudianteSencillo;
-import com.uce.edu.demo.estudiante.service.IEstudianteJpaService;
+import com.uce.edu.demo.repository.modelo.Ciudadano;
+import com.uce.edu.demo.repository.modelo.Empleado;
+import com.uce.edu.demo.service.ICiudadanoService;
 
 @SpringBootApplication
 public class ProyectoU2RrApplication implements CommandLineRunner {
@@ -18,7 +18,7 @@ public class ProyectoU2RrApplication implements CommandLineRunner {
 	private static final Logger logger = Logger.getLogger(ProyectoU2RrApplication.class);
 
 	@Autowired
-	private IEstudianteJpaService iEstudianteJpaService;
+	private ICiudadanoService ciudadanoService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2RrApplication.class, args);
@@ -27,15 +27,31 @@ public class ProyectoU2RrApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		List<EstudianteSencillo> listaEstSen = this.iEstudianteJpaService.buscarPorGeneroSencillo("M");
-		for (EstudianteSencillo item : listaEstSen) {
-			logger.info("Estudiante Sencillo: " + item);
-		}
+		Ciudadano ciudadano = new Ciudadano();
+		ciudadano.setNombre("Romina");
+		ciudadano.setApellido("Ramírez");
 
-		List<EstudianteContadorSemestre> listaEstContSem = this.iEstudianteJpaService.consultarCantidadPorSemestre("M");
-		for (EstudianteContadorSemestre item : listaEstContSem) {
-			logger.info("Cantidad por semestre: " + item);
-		}
+		Empleado empleado = new Empleado();
+		empleado.setCodigoIess("12323");
+		empleado.setSalario(new BigDecimal(100));
+		empleado.setCiudadano(ciudadano);
+
+		ciudadano.setEmpleado(empleado);
+
+		this.ciudadanoService.insertar(ciudadano);
+
+		Ciudadano ciudadano2 = new Ciudadano();
+		ciudadano2.setNombre("Maria");
+		ciudadano2.setApellido("Gomez");
+
+		Empleado empleado2 = new Empleado();
+		empleado2.setCodigoIess("45257");
+		empleado2.setSalario(new BigDecimal(200));
+		empleado2.setCiudadano(ciudadano2);
+
+		ciudadano.setEmpleado(empleado2);
+
+		// this.ciudadanoService.insertar(empleado2);
 
 	}
 
